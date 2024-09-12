f887406512578777c7f27d040f85b5b9c07682c3
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2024-09-12 13:18:51 -0400

    feat: initialize get and controls command

---
+++ cmd/get/get.go
@@ -0,0 +1,33 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package get
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var GetCmd = &cobra.Command{
+	Use:   "cli get controls|benchmarks --page-size",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("get called")
+	},
+}
+
+func init() {
+
+	GetCmd.AddCommand(controlsCmd)
+
+	GetCmd.PersistentFlags().Int("page-size", 25, "Defines page size of response")
+
+}
