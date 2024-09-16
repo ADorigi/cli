a30552f3d3d26fb0b34f57f32260ae7f057b925e
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2024-09-16 16:38:15 -0400

    fix: updated description for commands

--- cmd/configure.go
+++ cmd/configure.go
@@ -14,13 +14,14 @@ import (
 // configureCmd represents the configure command
 var configureCmd = &cobra.Command{
 	Use:   "configure",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Configuration for checkctl",
+	Long: `Configuration for checkctl
+	For interactive mode:
+		checkctl configure 
+	For non-interactive mode:
+		checktl configure --api-key <<api-key>> --app-endpoint https://path.to.app.endpoint --output json
+	`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 
 		var configuration *config.Configuration
--- cmd/get/controls.go
+++ cmd/get/controls.go
@@ -19,13 +19,11 @@ import (
 // controlsCmd represents the controls command
 var controlsCmd = &cobra.Command{
 	Use:   "controls",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Get information for all controls",
+	Long: `Get information for all controls
+	Example usage:
+		checkctl get controls --page-number 1 --page-size 20 --output json
+	`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		client := &http.Client{}
 		configuration, err := config.ReadConfigFile()
